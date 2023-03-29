c70c6adeb8ec4ab67567c1fda8f421c40427260a
a moazeni <55484136+magnumAv@users.noreply.github.com>
2023-03-29 05:09:41 +0430

    section 5

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Observable } from 'rxjs';
 import { User } from '../models/user';
 import { AccountService } from '../services/account.service';
@@ -10,27 +12,26 @@ import { AccountService } from '../services/account.service';
 })
 export class NavComponent implements OnInit {
   model: any = {};
-  constructor(public accountService: AccountService) {
-
-  }
-  ngOnInit(): void {
-
-  }
+  constructor(
+    public accountService: AccountService,
+    private router: Router,
+    private toaster: ToastrService
+  ) {}
+  ngOnInit(): void {}
 
   login() {
     this.accountService.login(this.model).subscribe(
       (s) => {
-        console.log(s);
-
+        this.router.navigateByUrl('/members');
       },
       (error) => {
         console.log(error);
+        this.toaster.error(error.error)
       }
     );
   }
   logout() {
     this.accountService.logout();
-
+    this.router.navigateByUrl('/');
   }
-
 }
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../services/account.service';
 
 @Component({
@@ -7,23 +8,25 @@ import { AccountService } from '../services/account.service';
   styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
-
   @Output() cancelRegister = new EventEmitter();
   ngOnInit(): void {}
   model: any = {};
-constructor(private accountService:AccountService){
-
-}
+  constructor(
+    private accountService: AccountService,
+    private toaseterService: ToastrService
+  ) {}
   register() {
-    this.accountService.register(this.model).subscribe(s=>{
-      this.cancel();
-
-    },error=>{
-      console.log(error);
-
-    })
+    this.accountService.register(this.model).subscribe(
+      (s) => {
+        this.cancel();
+      },
+      (error) => {
+        console.log(error);
+        this.toaseterService.error(error.error['title']);
+      }
+    );
   }
   cancel() {
-    this.cancelRegister.emit(false)
+    this.cancelRegister.emit(false);
   }
 }
