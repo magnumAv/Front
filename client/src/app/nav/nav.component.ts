b2faddbb03fcf0335d0f44cc71f604b067490706
a moazeni <55484136+magnumAv@users.noreply.github.com>
2023-03-30 03:40:11 +0430

    section 7

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -23,10 +23,6 @@ export class NavComponent implements OnInit {
     this.accountService.login(this.model).subscribe(
       (s) => {
         this.router.navigateByUrl('/members');
-      },
-      (error) => {
-        console.log(error);
-        this.toaster.error(error.error)
       }
     );
   }
